a903a56bd9a3800138b5f84111d00ead1d69cc3a
primus <pd@space.com>
2020-12-21 15:50:57 +0100

    20201221

--- note.py
+++ note.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
 '''
-note.py            add snippets to a file
-note.py -s regexp  search the the saved snippets
+Store all your text snippets to a file.
+Use keywords to find your snippets easily back
+I use it to store all kind of coding fragments.
+But also for installation instructions, and even for cooking recepts and all kind of things I always forget
+You can also edit the notefile with your favorite editor 
 
+note.py            add note snippets to a file
+note.py -s regexp  search items from the saved snippets
 
+edit '--dir' argument to set your note directory path permanent
+
+20201221 2.0  primus  now with multiple search items
 20201213 1.9  primus  with option to attach files to notes
 20201209 1.8  primus  rewritten as class
 20180918 1.7  primus  replace non ascii chars with "~"
 20161129 1.4  primus  split db file per year
-20150722 1.1  primus  argv argumenten included
+20150722 1.1  primus  keyword argumenten included
 20150715 1.0  primus  port from perl
 '''
-__version__ = '1.9'
+__version__ = '2.0'
 
 import re, os, sys
 import datetime
@@ -28,28 +36,23 @@ class notes(object):
     self.report = []
     self.stamp  = datetime.datetime.now().strftime("%Y%m%d-%H%M")
     self.year   = datetime.datetime.now().strftime('%Y')
-    self.dir = os.path.join('/Users/primus/notes')  # directory with note files
-    self.check_dir(self.dir)
-    self.db  = os.path.join(self.dir, 'notes{}.db'.format(self.year))    # current note file
-    self.filestore = os.path.join(self.dir, 'notes{}'.format(self.year)) # path to store appended files
+    self.check_dir(self.args.dir)
+    self.db  = os.path.join(self.args.dir, 'notes{}.db'.format(self.year))    # current note file
+    self.filestore = os.path.join(self.args.dir, 'notes{}'.format(self.year)) # path to store appended files
     self.check_dir(self.filestore)
-    for db in os.listdir(self.dir):
+    for db in os.listdir(self.args.dir):
       if re.search ('notes[0-9]{4}.db', db):
-        self.files.append(os.path.join(self.dir, db))
-    if self.args.search != '':   # search in notes
+        self.files.append(os.path.join(self.args.dir, db))
+    if self.args.search:           # search in notes
+      for i in self.args.keywords: # append all unspecified keyword arguments to the search items, all must be found in the snipped
+        self.args.search.append(i)
       for fn in self.files:
-        self.grepnote(fn) 
-    else:                      # add new note
+        self.searchfile(fn) 
+    else:                          # add new note
       self.newnote()
 
-  def add_report(self, h, b):
-     self.report.append(h)
-     self.report.append(b)
      
-  def check_dir(self, path):
-    '''
-    check if the directory exists otherwise create it
-    '''
+  def check_dir(self, path):       # check if the directory exists otherwise create it
     if not os.path.exists(path):
       try:
         os.mkdir(path)
@@ -57,16 +60,16 @@ class notes(object):
         logging.error('mkdir {} failed: {}'.format(path, e))
 
   def __repr__(self):
-     return ''.join(map(str,self.report))
+     return '\n'.join(map(str,self.report))
   
   def newnote(self):
-    hdr = '### {} {}'.format(self.stamp, ' '.join(map(str, self.args.argv)))
-    print('keywords {}'.format(' '.join(map(str, self.args.argv))))
+    hdr = '### {} {}'.format(self.stamp, ' '.join(map(str, self.args.keywords)))
+    print('keywords {}'.format(' '.join(map(str, self.args.keywords))))
     print('enter text for the new note, end with met ctrl-z or ctrl-d')
     with open(self.db, 'a') as fh:
       fh.write('\n{}\n'.format(hdr))
       for line in sys.stdin:
-        line=re.sub(r'[^\x09-\x7e]','~',line)  # strip all kind of junk
+#        line=re.sub(r'[^\x09-\x7e]','~',line)  # strip all kind of junk
         fh.write(line)
       if self.args.file:
         for i, fn in enumerate (self.args.file):
@@ -77,35 +80,47 @@ class notes(object):
           else:
             logging.error('file {} not found'.format(fn)) 
 
-  def grepnote(self, fn):
-    try:
-      blok, header, found = '', '', False
-      with open(fn, 'r') as fh:
-        for line in fh:
-          newblok = re.search('^###', line)
-          in_line = re.search(self.args.search, line, re.IGNORECASE)
-          if newblok:
-            if found:
-              self.add_report(header, blok)
-            blok, header, found = '', line, False
-          if in_line:
-            found = True
-          if (not newblok and ((self.args.line and in_line) or (not self.args.line))):
-            blok += line
-      if found:
-        self.add_report(header, blok)
-    except Exception as e:
-      logging.error('{}'.format(e))
+  def searchblok(self, blok):
+    hit = []
+    line_cnt = [0, ]
+    for j, s in enumerate(self.args.search):
+      hit.append(False)
+      for i ,line in enumerate(blok):
+        if re.search(s, line, re.I):   # ignore case
+          hit[j] = True
+          line_cnt.append(i)
+    for h in hit:
+      if not h:  # block must contain all search items
+        return
+    for i ,line in enumerate(blok):
+      if i in line_cnt or not self.args.line: # report line if it contains any of the search items or is the first line
+         self.report.append(line)
+  
+  def searchfile(self, fn):
+    blok = []
+    with open(fn, 'r') as fh:
+      for line in fh:
+        line=line.rstrip()
+        if re.search('^###', line):
+          self.searchblok(blok)
+          blok = [line, ]
+        else:
+          blok.append(line)
+      self.searchblok(blok)
 
 def parse_arguments():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-d', '--debug',     action='store_true')
-  parser.add_argument('-l', '--line',      action='store_true',  help='show only lines containing the search items')
-  parser.add_argument('-s', '--search',    type=str, default='', help='regular expression to search in notes')
-  parser.add_argument('-f', '--file',      action='append',      help='add one or more files to note')
-  parser.add_argument('-v', '--version',   action='version', version='%(prog)s ' + __version__)
-  parser.add_argument('argv', nargs='*')
+  parser = argparse.ArgumentParser('note snippets')
+  parser.add_argument('-d', '--debug',   action='store_true', help='debug mode')
+  parser.add_argument('-l', '--line',    action='store_true', help='show only lines containing the search items')
+  parser.add_argument('-s', '--search',  action='append',     help='add one or more search items, all items must be found in a snippet')
+  parser.add_argument('-f', '--file',    action='append',     help='add one or more files to note')
+  parser.add_argument('--dir', default='/Users/primus/notes', help='note directory path')   # edit this to make it permanent for your environment
+  parser.add_argument('-v', '--version', action='version',    version='%(prog)s ' + __version__)
+  parser.add_argument('keywords', nargs='*',                  help='create new note with these keywords')
   args = parser.parse_args()
+  if not args.keywords and not args.search:
+    parser.print_help()
+    exit()
   return args
 
 if __name__ == "__main__":
