1fc0958b634e2531b6bec8c17f74b1ba9d62d58e
primus <pd@space.com>
2025-03-14 19:45:07 +0100

    pylint

--- note.py
+++ note.py
@@ -6,8 +6,8 @@ I use it to store all kind of coding fragments.
 But also for installation instructions, and even for cooking recepts and all kind of things I always forget
 You can also edit the notefile with your favorite editor 
 
-note.py  keyword [keyword] ..         add note snippets to a file
-note.py -s searchstring               search items from the saved snippets
+note.py  keyword [keyword] ..              add note snippets to a file
+note.py -s searchstring [searchstring] ..  search items from the saved snippets
 
 You can edit the '--file' argument to your preferred file location and name
 
@@ -20,22 +20,31 @@ You can edit the '--file' argument to your preferred file location and name
 '''
 __version__ = '2.2'
 
-import re, os, sys
+import re
+import os
+import sys
 import datetime
 import argparse
 import string
 
-  
+
 def newnote(args):
+  '''
+  no search items were found in the arguments
+  so append a new snippet in the file
+  '''
   header = f'### {datetime.datetime.now().strftime("%Y%m%d-%H%M")} {' '.join(map(str, args.keywords))}'
   print('enter text for the new note, end with met ctrl-z or ctrl-d')
-  with open(args.file, 'a') as fh:
+  with open(args.file, 'a', encoding="utf-8") as fh:
     fh.write(f'\n{header}\n')
     for line in sys.stdin:
       line = ''.join(filter(lambda x: x in set(string.printable), line))
       fh.write(line)
 
 def searchsnippet(args, snippet):
+  '''
+  check if all search items exists in this snippet
+  '''
   hit = []
   line_cnt = [0, ]
   for j, s in enumerate(args.search):
@@ -49,49 +58,51 @@ def searchsnippet(args, snippet):
       return
   for i ,line in enumerate(snippet):
     if i in line_cnt or not args.line: # report line if it contains any of the search items or is the first line
-       print(line)
+      print(line)
 
 def searchfile(args):
+  '''
+  break the file into snippets
+  a snippet starts on a newline with ### followed by a datestamp
+  '''
   snippet = []
   cnt = 0
   with open(args.file, 'rb') as fh:
     for line in fh:
       cnt += 1
       try:
-        line = line.decode('utf-8', 'replace')
-        line = line.rstrip()
+        line = line.decode('utf-8', 'replace').rstrip()
         line = ''.join(filter(lambda x: x in set(string.printable), line))
       except Exception as e:
-        line = f'problem decoding to utf-8 in file {args.file} line {cnt}'
+        line = f'problem decoding to utf-8 in file {args.file} line {cnt} {e}'
         print(line)
       if re.search('^### [0-9]{6}', line):
         searchsnippet(args, snippet)
         snippet = [line, ]
       else:
         snippet.append(line)
-  searchsnippet(args, snippet)  
+  searchsnippet(args, snippet)
 
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser('note snippets')
   parser.add_argument('-l', '--line',    action='store_true',   help='show only lines containing the search items')
   parser.add_argument('-s', '--search',  action='append',       help='add one or more search items, all items must be found in a snippet')
-  parser.add_argument('-f', '--file',    type=str, default='notes.txt', help='file to store the snippets, change this to your preferred file location')  
+  parser.add_argument('-f', '--file',    type=str, default='notes.txt', help='file to store the snippets, change this to your preferred file location')
   parser.add_argument('-v', '--version', action='version',      version='%(prog)s ' + __version__)
   parser.add_argument('keywords', nargs='*',                    help='create new note with these keywords')
   args = parser.parse_args()
-  
+
   if not args.keywords and not args.search:
     parser.print_help()
-    exit()
+    sys.exit()
 
   if not os.path.isfile(args.file):
     if not input(f'file {args.file} does not exist, start with an empty file (Y/N)').lower() in ('yes','y','ja','j'):
-      exit()
+      sys.exit()
 
   if args.search:                      # search in notes
     args.search.extend(args.keywords)  # extend all unspecified keyword arguments to the search items, all must be found in the snipped
-    searchfile(args) 
+    searchfile(args)
   else:                                # add new note
     newnote(args)
-
